ca1419765ed844b1a5f8a206ec7d3700c62124c2
ChristmasterX <116984266+ChristmasterX@users.noreply.github.com>
2022-10-30 12:42:36 +0000

    Update settings.js

--- settings.js
+++ settings.js
@@ -19,11 +19,11 @@ global.APIKeys = {
 }
 
 //customize settings\\
-global.owner = ['Your Number']
-global.premium = ['Your Number']
-global.ownernomer = 'Your Number'
-global.ownername = '🐦Star Boy Nexus'
-global.botname = 'Gojo-Satoru'
+global.owner = ['0153374305']
+global.premium = ['0153374305']
+global.ownernomer = '0153374305'
+global.ownername = 'Christmaster'
+global.botname = 'Anya'
 global.footer = '©NexusNwInc.'
 global.ig = 'https://github.com/nexusNw'
 global.region = 'India, South India, Kerala'
